7cc478e3cb9fdbef65dcf41abbe46f5009a3d00e
NOknow95 <1272657070@qq.com>
2020-05-08 09:35:18 +0800

    建造者模式；

--- src/com/jw/mode/learning/builder/Client.java
+++ src/com/jw/mode/learning/builder/Client.java
@@ -12,11 +12,11 @@ public class Client {
 
   public static void main(String[] args) {
     Director director = new DefaultDirector(new HtmlArticleBuilder());
-//    Director director = new DefaultDirector(new TextArticleBuilder());
     String article = director.buildArticle("title", Arrays.asList("11", "222"), "this is a test.");
     System.out.println(article);
-
-
+    director = new DefaultDirector(new TextArticleBuilder());
+    article = director.buildArticle("title", Arrays.asList("11", "222"), "this is a test.");
+    System.out.println(article);
   }
 
 }
